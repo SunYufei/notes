251283710bede8f4446f17a1061da7b5101fb288
SunYufei <sunyufei963@qq.com>
2023-12-07 13:58:26 +0800

    [spring boot] AOP 接口调用日志

---
+++ spring-boot-demo/common/src/main/java/ml/sun/common/aspect/LoggingAspect.kt
@@ -0,0 +1,47 @@
+package ml.sun.common.aspect
+
+import ml.sun.common.ext.LogExt.logger
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.AfterThrowing
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.LocalDateTime
+
+@Aspect
+@Component
+class LoggingAspect {
+    @Pointcut(
+        "within(@org.springframework.stereotype.Repository *)" +
+                "|| within(@org.springframework.stereotype.Service *)" +
+                "|| within(@org.springframework.web.bind.annotation.RestController *)"
+    )
+    fun pointcut() {
+    }
+
+    @Around("pointcut()")
+    fun around(joinPoint: ProceedingJoinPoint): Any {
+        val className = joinPoint.signature.declaringTypeName
+        val methodName = joinPoint.signature.name
+        logger.debug("进入方法 {}.{}()，参数列表 {}", className, methodName, joinPoint.args)
+        val begin = LocalDateTime.now()
+        val result = joinPoint.proceed()
+        val end = LocalDateTime.now()
+        val duration = Duration.between(begin, end).toMillis()
+        logger.debug("方法结束 {}.{}()，耗时 {}ms，返回值 {}", className, methodName, duration, result)
+        return result
+    }
+
+    @AfterThrowing(pointcut = "pointcut()", throwing = "e")
+    fun afterThrowing(joinPoint: JoinPoint, e: Throwable) =
+        logger.error(
+            "产生异常 {}.{}()，异常内容 {} {}",
+            joinPoint.signature.declaringTypeName,
+            joinPoint.signature.name,
+            e.cause,
+            e.message
+        )
+}
